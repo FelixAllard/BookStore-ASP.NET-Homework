b451d3be0f7a38faaddcbce4a55e266544c6fc2b
Felix Allard <xilef992@gmail.com>
2024-08-29 20:55:13 -0400

    Working individual book bugfix

--- OnlineBookstoreAssignment/Models/Book.cs
+++ OnlineBookstoreAssignment/Models/Book.cs
@@ -6,6 +6,7 @@ public class Book
 {
     [Key]
     public int Id { get; set; }
+    public string ISBN { get; set; }
     public string Title { get; set; }
     public string Author { get; set; }
     public string Publisher { get; set; }
